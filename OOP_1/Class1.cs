108c885ac56396538dfdf46e1add3440e27db879
mjndfreakk <cagdasergencc@gmail.com>
2024-08-04 22:04:11 +0300

    Add project files.

---
+++ OOP_1/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_1
+{
+    public class Person
+    {
+        
+        public string name;
+        public string surname;
+        public string birthDate;
+        public string profession;
+
+
+        public string Name 
+        { 
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (name != string.Empty)
+                { 
+                    name = value;
+                }
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                if (surname != string.Empty)
+                {
+                    surname = value;
+                }
+            }
+        }
+
+        public string BirthDate
+        {
+            get
+            {
+                return birthDate;
+            }
+            set
+            {
+                if (birthDate != string.Empty)
+                {
+                    birthDate = value;
+                }
+            }
+        }
+
+        public string Profession
+        {
+            get
+            {
+                return profession;
+            }
+            set
+            {
+                if (profession != string.Empty)
+                {
+                    profession = value;
+                }
+            }
+        }
+
+        public void DisplayPerson()
+        {
+            Console.WriteLine(@"
+            Name: " + name + " Surname: " + surname + " Birth date: " + birthDate + " Profession: " + profession + "");
+        }
+    }
+}
---
+++ OOP_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using OOP_1;
+
+Person person1 = new Person();
+person1.Name = "Cagdas";
+person1.Surname = "Ergenc";
+person1.BirthDate = "19/09/96";
+person1.Profession = "Student";
+
+
+Person person2 = new Person();
+person2.Name = "Abdülrezzak";
+person2.Surname = "Gülençayır";
+person2.BirthDate = "02/09/12";
+person2.Profession = "Teacher";
+
+
+Person person3 = new Person();
+person3.Name = "Müslüm";
+person3.Surname = "Anoğlu";
+person3.BirthDate = "22/07/81";
+person3.Profession = "Teacher";
+
+person1.DisplayPerson();
+person2.DisplayPerson();
+person3.DisplayPerson();
